6dfec43348cf100d043aab3101b66aa304be124c
Samson Omondi <ksamy90@aol.com>
2018-05-18 22:09:08 -0700

    Edit tests for Edit expenses files

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
     removeExpense, 
     setExpenses, 
     startSetExpenses, 
-    startRemoveExpense 
+    startRemoveExpense,
+    startEditExpense 
 } from '../../actions/expenses';
 
 import expenses from '../fixtures/expenses';
@@ -53,6 +54,24 @@ test('should show edit expense action object', () => {
         type: 'EDIT_EXPENSE',
         id: '1040ez',
         updates: { description: 'Seattle' }
+    });
+});
+
+test('should edit expense from firebase', (done) => {
+    const store = createMockStore({});
+    const id = expenses[0].id;
+    const updates = { amount: 35000 };
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
     });
 });
 

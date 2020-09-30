2ea18bac960815c3d0898823896fe3cb3024c994
ksamy90 <ksamy90@aol.com>
2020-09-30 10:57:56 +0300

    edit redux store configuration

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,20 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import expensesReducer from '../reducers/expenses';
-import filtersReducer from '../reducers/filters';
-import authReducer from '../reducers/auth';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import expensesReducer from "../reducers/expenses";
+import filtersReducer from "../reducers/filters";
+import authReducer from "../reducers/auth";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Store Creation
 export default () => {
-    const store = createStore(
-        combineReducers({
-            expenses: expensesReducer,
-            filters: filtersReducer,
-            auth: authReducer
-        }),
-        composeEnhancers(applyMiddleware(thunk))
-    );
-    return store;
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer,
+      auth: authReducer,
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
+  return store;
 };
